42953f87d2542db9a5d3555f22d016aab8143e0b
Roman Pushkov <pushkovroman@me.com>
2020-06-26 16:47:16 +0300

    MSPF-487: graceful shutdown (#80)
    * add graceful shutdown tests
    
    * add drainer
    
    * add drainer to tree
    
    * use drainer lib
    
    * update .app.src
    
    * cleanup
    
    * update dep to http
    
    * cleanup

--- apps/capi/src/capi_swagger_server.erl
+++ apps/capi/src/capi_swagger_server.erl
@@ -6,6 +6,7 @@
 -define(DEFAULT_ACCEPTORS_POOLSIZE, 100).
 -define(DEFAULT_IP_ADDR, "::").
 -define(DEFAULT_PORT, 8080).
+-define(RANCH_REF, ?MODULE).
 
 -define(START_TIME_TAG, processing_start_time).
 
@@ -16,7 +17,16 @@
 child_spec({HealthRoutes, LogicHandler}) ->
     {Transport, TransportOpts} = get_socket_transport(),
     CowboyOpts = get_cowboy_config(HealthRoutes, LogicHandler),
-    ranch:child_spec(?MODULE, Transport, TransportOpts, cowboy_clear, CowboyOpts).
+    GsTimeout = genlib_app:env(?APP, graceful_shutdown_timeout, 5000),
+    Protocol = cowboy_clear,
+    cowboy_draining_server:child_spec(
+        ?RANCH_REF,
+        Transport,
+        TransportOpts,
+        Protocol,
+        CowboyOpts,
+        GsTimeout
+    ).
 
 get_socket_transport() ->
     {ok, IP}      = inet:parse_address(genlib_app:env(?APP, ip, ?DEFAULT_IP_ADDR)),
