2227a08c2242c5b2929571cba1409d0d277ca833
Andrew Mayorov <encube.ul@gmail.com>
2022-02-03 17:21:32 +0300

    Add eunit test on json marshalling

--- apps/capi/src/capi_json_marshalling.erl
+++ apps/capi/src/capi_json_marshalling.erl
@@ -48,3 +48,53 @@ unmarshal({obj, Object}) ->
     maps:fold(fun(K, V, Acc) -> maps:put(K, unmarshal(V), Acc) end, #{}, Object);
 unmarshal({arr, Array}) ->
     lists:map(fun unmarshal/1, Array).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+-spec test() -> _.
+
+-define(INSTANCES, [
+    {undefined, {nl, #json_Null{}}},
+    {42, {i, 42}},
+    {false, {b, false}},
+    {
+        #{
+            <<"leet">> => 1.337,
+            <<"there">> => [<<"be">>, <<"🐲"/utf8>>, <<"dragons">>],
+            <<"this">> => #{<<"is">> => true}
+        },
+        {obj, #{
+            <<"leet">> => {flt, 1.337},
+            <<"there">> => {arr, [{str, <<"be">>}, {str, <<"🐲"/utf8>>}, {str, <<"dragons">>}]},
+            <<"this">> => {obj, #{<<"is">> => {b, true}}}
+        }}
+    }
+]).
+
+-spec marshalling_test_() -> _.
+marshalling_test_() ->
+    [?_assertEqual(marshal(Instance), Marshalled) || {Instance, Marshalled} <- ?INSTANCES].
+
+-spec unmarshalling_test_() -> _.
+unmarshalling_test_() ->
+    [?_assertEqual(Instance, unmarshal(Marshalled)) || {Instance, Marshalled} <- ?INSTANCES].
+
+-spec symmetric_marshalling_test_() -> _.
+symmetric_marshalling_test_() ->
+    [?_assertEqual(Instance, unmarshal(marshal(Instance))) || {Instance, _} <- ?INSTANCES].
+
+-spec symmetric_unmarshalling_test_() -> _.
+symmetric_unmarshalling_test_() ->
+    [?_assertEqual(Marshalled, marshal(unmarshal(Marshalled))) || {_, Marshalled} <- ?INSTANCES].
+
+-spec thrift_serialize_test_() -> _.
+thrift_serialize_test_() ->
+    [?_test(serialize_thrift(marshal(Instance))) || {Instance, _} <- ?INSTANCES].
+
+serialize_thrift(Term) ->
+    C1 = thrift_strict_binary_codec:new(),
+    C2 = thrift_strict_binary_codec:write(C1, {struct, union, {dmsl_json_thrift, 'Value'}}, Term),
+    thrift_strict_binary_codec:close(C2).
+
+-endif.
