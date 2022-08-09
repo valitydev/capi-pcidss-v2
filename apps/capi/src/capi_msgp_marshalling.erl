fdba4db38396276a653275cbd4ae6308fcbb6be9
Andrew Mayorov <encube.ul@gmail.com>
2022-08-10 00:21:21 +0300

    Add msgpack marshalling tests

--- apps/capi/src/capi_msgp_marshalling.erl
+++ apps/capi/src/capi_msgp_marshalling.erl
@@ -52,3 +52,53 @@ unmarshal({obj, Object}) ->
     maps:fold(fun(K, V, Acc) -> maps:put(unmarshal(K), unmarshal(V), Acc) end, #{}, Object);
 unmarshal({arr, Array}) ->
     lists:map(fun unmarshal/1, Array).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+-spec test() -> _.
+
+-define(INSTANCES, [
+    {undefined, {nl, #msgpack_Nil{}}},
+    {42, {i, 42}},
+    {false, {b, false}},
+    {
+        #{
+            3.1415 => 1.337,
+            <<"there">> => [<<"be">>, {bin, <<"🐲"/utf8>>}, <<"dragons">>],
+            false => #{<<"is">> => true}
+        },
+        {obj, #{
+            {flt, 3.1415} => {flt, 1.337},
+            {str, <<"there">>} => {arr, [{str, <<"be">>}, {bin, <<"🐲"/utf8>>}, {str, <<"dragons">>}]},
+            {b, false} => {obj, #{{str, <<"is">>} => {b, true}}}
+        }}
+    }
+]).
+
+-spec marshalling_test_() -> _.
+marshalling_test_() ->
+    [?_assertEqual(marshal(Instance), Marshalled) || {Instance, Marshalled} <- ?INSTANCES].
+
+-spec unmarshalling_test_() -> _.
+unmarshalling_test_() ->
+    [?_assertEqual(Instance, unmarshal(Marshalled)) || {Instance, Marshalled} <- ?INSTANCES].
+
+-spec symmetric_marshalling_test_() -> _.
+symmetric_marshalling_test_() ->
+    [?_assertEqual(Instance, unmarshal(marshal(Instance))) || {Instance, _} <- ?INSTANCES].
+
+-spec symmetric_unmarshalling_test_() -> _.
+symmetric_unmarshalling_test_() ->
+    [?_assertEqual(Marshalled, marshal(unmarshal(Marshalled))) || {_, Marshalled} <- ?INSTANCES].
+
+-spec thrift_serialize_test_() -> _.
+thrift_serialize_test_() ->
+    [?_test(serialize_thrift(marshal(Instance))) || {Instance, _} <- ?INSTANCES].
+
+serialize_thrift(Term) ->
+    C1 = thrift_strict_binary_codec:new(),
+    {ok, C2} = thrift_strict_binary_codec:write(C1, {struct, union, {dmsl_msgpack_thrift, 'Value'}}, Term),
+    thrift_strict_binary_codec:close(C2).
+
+-endif.
